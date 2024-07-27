729be9e9b38d7e6f9fe2d5cad4a174e558301ca6
Kimokichi <166203583+Kimokichi@users.noreply.github.com>
2024-07-27 13:29:14 +0700

    Add files via upload

--- src/scenes/MathFighterScene.js
+++ src/scenes/MathFighterScene.js
@@ -9,6 +9,27 @@ export default class MathFighterScene extends Phaser.Scene{
         this.player = undefined
         this.enemy = undefined
         this.slash = undefined
+        this.startGame = undefined
+        this.questionText = undefined
+        this.resultText = undefined
+        this.button1 = undefined
+        this.button2 = undefined
+        this.button3 = undefined
+        this.button4 = undefined
+        this.button5 = undefined
+        this.button6 = undefined
+        this.button7 = undefined
+        this.button8 = undefined
+        this.button9 = undefined
+        this.button0 = undefined
+        this.buttonDel = undefined
+        this.buttonOk = undefined
+        this.numberArray = []
+        this.number = 0
+        this.question = []
+        this.correctAnswer = undefined
+        this.playerAttack = false
+        this.enemyAttack = false
     }
     preload(){
         this.load.image('background','images/bg_layer1.png')
@@ -26,6 +47,7 @@ export default class MathFighterScene extends Phaser.Scene{
         this.load.spritesheet('slash','images/slash.png',{
             frameHeight: 88, frameWidth: 42
         })
+        this.load.image('start-btn','images/start_button.png')
     }
     create(){
         this.add.image(240,320,'background')
@@ -44,15 +66,51 @@ export default class MathFighterScene extends Phaser.Scene{
         this.physics.add.collider(this.enemy,tile)
         this.slash = this.physics.add.sprite(240,60,'slash')
         .setActive(false)
-        .setVisible(true)
+        .setVisible(false)
         .setGravityY(-500)
         .setOffset(0,-10)
         .setDepth(1)
         .setCollideWorldBounds(true)
         this.createAnimation()
+        let start_button = this.add.image(this.gameHalfWidth,this.gameHalfHeight + 181,'start-btn').setInteractive()
+        start_button.on('pointerdown', () => {
+          this.gameStart()
+          start_button.destroy()
+        }, this)
+        this.physics.add.overlap(
+          this.slash,
+          this.player,
+          this.spriteHit,
+          null,
+          this
+        )
+        this.physics.add.overlap(
+          this.slash,
+          this.enemy,
+          this.spriteHit,
+          null,
+          this
+        )
     }
-    update(){
+    update(time){
+      if(this.correctAnswer === true && !this.playerAttack){
+        this.player.anims.play('player-attack',true)
+        this.time.delayedCall(500, () => {
+          this.createSlash(this.player.x+60,this.player.y,4,600)
+        })
+        this.playerAttack = true
+      }
+      if (this.correctAnswer === undefined){
+        this.player.anims.play('player-standby',true)
+        this.enemy.anims.play('enemy-standby',true)
 
+      }
+      if(this.correctAnswer === false && !this.enemyAttack){
+        this.enemy.anims.play('enemy-attack',true)
+        this.time.delayedCall(500, () =>{
+          this.createSlash(this.enemy.x-60,this.enemy.y,2,-600,true)
+        })
+      }
     }
     createAnimation(){
         //player Animations
@@ -106,6 +164,142 @@ export default class MathFighterScene extends Phaser.Scene{
         frames : this.anims.generateFrameNumbers('enemy',
                 {start : 0, end : 4}),
         frameRate: 10,
+      })
+    }
+    gameStart(){
+      this.startGame = true
+      this.player.anims.play('player-standby',true)
+      this.enemy.anims.play('enemy-standby', true)
+      // @ts-ignore
+      this.resultText = this.add.text(this.gameHalfWidth,200,'0',{fontSize : '40px', fill : 'maroon', fontFamily : 'Arial, sans-serif'})
+      // @ts-ignore
+      this.questionText = this.add.text(this.gameHalfWidth,100,'0',{fontSize : '40px', fill : 'maroon', fontFamily : 'Arial, sans-serif'})
+      this.createButtons()
+      this.input.on('gameobjectdown', this.addNumbers, this)
+      this.generateQuestion()
+    }
+    createButtons(){
+      const startPosY = this.scale.height - 246
+      const widthDiff = 131
+      const heightDiff = 71.25
+
+      //center buttons
+      this.button2 = this.add.image(this.gameHalfWidth,startPosY,'numbers',1).setInteractive().setData('value',2)
+      this.button5 = this.add.image(this.gameHalfWidth,this.button2.y + heightDiff,'numbers',4).setInteractive().setData('value',5)
+      this.button8 = this.add.image(this.gameHalfWidth,this.button5.y + heightDiff,'numbers',7).setInteractive().setData('value',8)
+      this.button0 = this.add.image(this.gameHalfWidth,this.button8.y + heightDiff,'numbers',10).setInteractive().setData('value',0)
+
+      //left buttons
+      this.button1 = this.add.image(this.button2.x - widthDiff,startPosY,'numbers',0).setInteractive().setData('value',1)
+      this.button4 = this.add.image(this.button5.x - widthDiff,this.button1.y + heightDiff,'numbers',3).setInteractive().setData('value',4)
+      this.button7 = this.add.image(this.button8.x - widthDiff,this.button4.y + heightDiff,'numbers',6).setInteractive().setData('value',7)
+      this.buttonDel = this.add.image(this.button0.x - widthDiff,this.button7.y + heightDiff,'numbers',9).setInteractive().setData('value','del')
+
+      //right buttons
+      this.button3 = this.add.image(this.button2.x + widthDiff,startPosY,'numbers',2).setInteractive().setData('value',3)
+      this.button6 = this.add.image(this.button5.x + widthDiff,this.button3.y + heightDiff,'numbers',5).setInteractive().setData('value',6)
+      this.button9 = this.add.image(this.button8.x + widthDiff,this.button6.y + heightDiff,'numbers',8).setInteractive().setData('value',9)
+      this.buttonOk = this.add.image(this.button0.x + widthDiff,this.button9.y + heightDiff,'numbers',11).setInteractive().setData('value','ok')
+    }
+    addNumbers(pointer,object,event){
+      let value = object.getData('value')
+
+      if(isNaN(value)){
+        if(value == 'del'){
+          this.numberArray.pop()
+          if(this.numberArray.length < 1) {
+            this.numberArray[0] = 0
+          }
+        }
+        if(value == 'ok'){
+          this.checkAnswer()
+          this.numberArray = []
+          this.numberArray[0] = 0
+        }
+      }
+      else{
+        if(this.numberArray.length == 1 && this.numberArray[0] == 0){
+                this.numberArray[0] = value
+        } else {
+          if(this.numberArray.length < 10){
+            this.numberArray.push(value)
+          }
+        }
+      }
+      this.number = parseInt(this.numberArray.join(''))
+      // @ts-ignore
+      this.resultText.setText(this.number)
+      const textHalfWidth = this.resultText.width * 0.5
+      this.resultText.setX(this.gameHalfWidth - textHalfWidth)
+      event.stopPropagation()
+    }
+    getOperator(){
+      const operator = ['+', '-', 'x', ':']
+      return operator[Phaser.Math.Between(0,3)]
+    }
+    generateQuestion(){
+      let numberA = Phaser.Math.Between(0,50)
+      let numberB = Phaser.Math.Between(0,50)
+      let operator = this.getOperator()
+      if (operator === '+'){
+        this.question[0] = `${numberA}+${numberB}`
+        this.question[1] = numberA + numberB
+      }
+      if (operator === 'x'){
+        this.question[0] = `${numberA}x${numberB}`
+        this.question[1] = numberA * numberB
+      }
+      if (operator === '-'){
+        if(numberB > numberA){
+          this.question[0] = `${numberB}-${numberA}`
+          this.question[1] = numberB - numberA
+        }else{
+          this.question[0] = `${numberA}-${numberB}`
+          this.question[1] = numberA - numberB
+        }
+      }
+      if (operator === ':'){
+        do{
+          numberA = Phaser.Math.Between(0,50)
+          numberB = Phaser.Math.Between(0,50)
+        }while(!Number.isInteger(numberA / numberB))
+          this.question[0] = `${numberA}:${numberB}`
+          this.question[1] = numberA / numberB
+      }
+      this.questionText.setText(this.question[0])
+      const textHalfWidth = this.questionText.width * 0.5
+      this.questionText.setX(this.gameHalfWidth - textHalfWidth)
+    }
+    checkAnswer(){
+      if(this.number == this.question[1]){
+        this.correctAnswer = true
+      } else{
+        this.correctAnswer = false
+      }
+    }
+    createSlash(x,y,frame,velocity,flip = false){
+        this.slash.setPosition(x,y)
+        .setActive(true)
+        .setVisible(true)
+        .setFrame(frame)
+        .setFlipX(flip)
+        .setVelocityX(velocity)
+    }
+    spriteHit(slash,sprite){
+      slash.x = 0
+      slash.y = 0
+      slash.setActive(false)
+      slash.setVisible(false)
+      if(sprite.texture.key == 'player'){
+        sprite.anims.play('player-hit',true)
+      }else{
+        sprite.anims.play('enemy-hit',true)
+      }
+      this.time.delayedCall(500, () => {
+         this.playerAttack = false
+         this.enemyAttack = false
+         this.correctAnswer = undefined
+         this.generateQuestion()
       })
     }
 }
